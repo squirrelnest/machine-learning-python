b4061405bf35f8d299961adfa93ce8531efe99e5
mikka <mikkanthrope@gmail.com>
2018-08-10 18:39:04 -0700

    fundamentals and numpy KNN

---
+++ numpy-KNN.py
@@ -0,0 +1,239 @@
+#************************************************#
+# 1. Numpy Basics
+#************************************************#
+
+# The following line imports the numpy library
+import numpy as np
+
+# A. Use np.array to turn a_python_list into a numpy array
+#    Store the result in the variable a_numpy_array
+a_python_list = [[1,2,3], [4,5,6]]
+
+a_numpy_array = np.array(a_python_list)
+
+# Uncomment the "assert" statements below to test your code.
+# If an error is thrown, your code is wrong. If nothing happens, the test is passing.
+assert type(a_numpy_array) is np.ndarray
+assert np.array_equal(a_numpy_array, a_python_list)
+
+
+# B. Create a numpy array with shape (2,4,3).
+#    Store the result in the variable array_243
+
+a=1
+b=2
+c=3
+
+array_243 = np.array(
+                  [
+                    [
+                      [a,b,c],
+                      [a,b,c],
+                      [a,b,c],
+                      [a,b,c],
+                    ],
+                    [
+                      [a,b,c],
+                      [a,b,c],
+                      [a,b,c],
+                      [a,b,c],
+                    ]
+                  ])
+
+# Uncomment the "assert" statement below to test your code.
+assert array_243.shape == (2,4,3)
+# print(array_243.shape)
+
+# C. Use array slicing to create a copy of the first two rows
+# and the first two columns of `indexing_ex`. Store the result
+# in the variable `subset_22`
+indexing_ex = np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+])
+
+subset_22 = indexing_ex[0:2,0:2]
+
+assert np.array_equal(subset_22, [[1,2],[4,5]])
+
+# D. Use `np.zeros` to create an array of zeros with shape `(2,2)`
+# Store the result in the variable arr_22
+
+s=(2,2)
+arr_22 = np.zeros(s)
+
+# print(arr_22.shape)
+
+# E. use np.random.uniform to create a numpy array with shape
+# (5000,) filled with random values between -1 and 1
+# Store the result to a variable, uniformly_random
+
+random_shape = np.random.uniform(1,10,5000)
+uniformly_random = np.array(random_shape)
+
+# print(uniformly_random.shape)
+
+# F. Use np.random.normal to create a numpy array with shape
+# (5000,) filled with normally distributed random values with
+# mean 0 and standard deviation 1. Save the result to a
+# variable, normal_random. (for more info on normal distributions,
+# see https://en.wikipedia.org/wiki/Normal_distribution)
+
+normal_shape = np.random.normal(0,1,5000)
+normal_random = np.array(normal_shape)
+
+stdev = np.std(normal_random)
+
+# print(normal_random.shape)
+# print(stdev)
+
+#************************************************#
+# 2. Distance between points
+#************************************************#
+
+# The distance function accepts two parameters:
+#     point_a: a numpy array with shape (2,)
+#     point_b: a numpy array with shape (2,)
+# We will think about point_a and point_b as if they are points in an (x,y) coordinate plane.
+# The distance function will return a non-negative number that represents the distance between
+# point_a and point_b.
+
+def distance(point_a, point_b):
+  # A. Use subtraction to find the "difference" between point_a and point_b.
+  #    Save the result to the variable "diff"
+  diff = np.subtract(point_a, point_b)
+
+  # B. We need to measure the length of the vector "diff"
+  #    Start by squaring each coordinate in diff. Save the result to "diff_squared"
+  diff_squared = np.square(diff)
+
+
+  # C. Now that we've squared each of the coordinates in diff, we have to add them up.
+  #    Use np.sum to compute the sum of all of the coordinates in "diff_squared".
+  #    Save the result to "squared_coord_sum"
+  squared_coord_sum = np.sum(diff_squared)
+  # D. Last, we need to compute the square root of "squared_coord_sum".
+  #    Hint: look into np.sqrt
+  #    save the result to the variable "dist"
+  dist = np.sqrt(squared_coord_sum)
+  # E. Now return the distance between point_a and point_b that we just calculated
+  return dist
+  # pass
+
+# print(distance(np.array([0,0]), np.array([3,4])))
+# uncomment the following assert statements to test your code
+assert distance(np.array([0,0]), np.array([3,4])) == 5
+assert distance(np.array([-3, -1]), np.array([-8, 11])) == 13
+
+# Note: if you wrote your function using numpy operations and
+# methods instead of for loops, you function should work for
+# numpy vectors with shape (n,) for any number n!
+# uncomment below to test:
+# assert distance(np.array([0,0,0]), np.array([2,3,6])) == 7
+# assert distance(np.array([6, -2, 3]), np.array([2, 2, 10])) == 9
+
+#************************************************#
+# 3. get_distances
+#************************************************#
+
+# get_distances will be similar to the distance function we created earlier.
+# get_distances accepts two parameters:
+#     points: a numpy array with shape (n, 2). You can think of points as a
+#             list of points with shape (2,)
+#     base_point: a numpy array with shape (2,).
+# The goal of get_distances is to return a numpy array where each element
+# the distance between one of the points in "points" and "base_point".
+# Consider the example below:
+
+ex_points = np.array([
+  [3,4],
+  [5,12],
+  [9, 40]
+])
+
+ex_base_point = np.array([0,0])
+
+# get_distances(ex_points, ex_base_point) should be equal to np.array([5, 13, 41])
+# since the distance between [3,4] and [0,0] is 5; the distance between
+# [5, 12] and [0,0] is 13; and the distance between [9, 40] and [0,0] is 41.
+
+def get_distances(points, base_point):
+  # A. Use subtraction to create an array of shape (n, 2) that holds a list of
+  #    differences between the points in "points" and "base_point". Save the
+  #    result to diffs.
+  diffs = np.subtract(points, base_point)
+  # print(diffs)
+  # B. Square all the coordinates in diffs. Save the result to diffs_coords_squared.
+  diffs_coords_squared = np.square(diffs)
+  # print(diffs_coords_squared)
+  # C. Use np.sum to sum all of the numbers IN EACH ROW of diffs_coords_squared.
+  #    This should result in an array with shape (n,). If you aren't careful, you may
+  #    end up summing every number in the whole array. Save the result to "sums"
+  #    Hint: look up the numpy docs for np.sum. what does the keyword argument "axis" do?
+  sums = np.sum(diffs_coords_squared, axis=1)
+  # print(sums)
+  # D. Use np.sqrt to find the square root of each number in sums. Save the result to "dists"
+  dists = np.sqrt(sums)
+  # E. Now return dists!
+  return dists
+
+# Test your code using the example provided before the function definition
+
+# print(get_distances(ex_points, ex_base_point))
+
+#************************************************#
+# Extension: knn_predict
+#************************************************#
+
+# knn_predict will accept three parameters:
+#     animal_data: a numpy array with shape (n, 2) that represents a dataset of animals.
+#         Each row is a different animal. The first column is the animals domestication level
+#         and the second column is the animal's size.
+#     animal_types: a numpy array with shape (n,) that represents the type of animal each animal
+#         in animal_data is. (E.g. if the first row in animal_data is a cat, then the first entry in
+#         animal_types will be 'cat')
+#     unknown_animal: a numpy array with shape (2,). The first number is its domestication level.
+#         The second number is its size.
+# The goal of knn_predict is to predict whether unknown animal is a cat or a dog. knn_predict
+# should output either 'cat' or 'dog'.
+
+def knn_predict(animal_data, animal_types, unknown_animal):
+  # A. Use get_distances to find the distance between the unkown_animal and all of the
+  #    animals in animal_data.
+  distances_from_unknown = get_distances(unknown_animal, animal_data)
+
+  # B. Find which type of animal the three closest animals in animal_data are.
+  K = 3
+  closest_indices = np.argpartition(distances_from_unknown,-K)[-K:]
+  closest = np.array(animal_types[closest_indices])
+
+  # C. Return 'cat' if at least two are cats. Return 'dog' if at least two are dogs.
+
+  num_cats = (closest == 'cat').sum()
+  num_dogs = (closest == 'dog').sum()
+
+  if num_cats >= 2:
+    print('cat')
+    return 'cat'
+  elif num_dogs >= 2:
+    print('dog')
+    return 'dog'
+
+
+animal_data = np.array([
+  [3,4],
+  [5,12],
+  [9, 40],
+  [18, 3],
+  [7, 23],
+  [5, 6]
+])
+
+animals = ['cat', 'cat', 'cat', 'dog', 'dog', 'dog']
+
+animal_types = np.array(animals, dtype=object)
+
+unknown_animal = np.array([6, 14])
+
+knn_predict(animal_data, animal_types, unknown_animal)
---
+++ python-fundamentals.py
@@ -0,0 +1,225 @@
+# Welcome to Python for Machine Learning!
+# Let's begin reviewing some features of python Below
+# The instructions on the right contain additional information that may
+# be helpful for completing the challenges
+
+# We'll use this function to test our code
+def test(challenge, val):
+  assert val , "Challenge {i} failing".format(i=challenge)
+  print("Challenge {i} passing".format(i=challenge))
+
+# *************************************
+# *************************************
+
+# 1. Comments
+# Comments in Python begin with a hash
+# Comments allow us to explain our code
+# The python interpreter ignores these lines
+
+# *************************************
+# *************************************
+
+# 2. Variables
+# An example of creating a variable:
+my_number = 10
+
+# Use the print function to display variables on the console below
+print('my_number is: ', my_number)
+
+# 2.1 Create a variable, another_number, that stores the value 11
+
+another_number = 11
+# Uncomment below to test 2.1
+test('2.1', another_number == 11)
+
+# 2.2 Use the print function to display the value of another_number below
+# There's no test for 2.2
+
+
+# 2.3 Reassign the variable another_number to the value 11.5
+
+another_number = 11.5
+
+# Uncomment below to test 2.3
+test('2.3', another_number == 11.5)
+
+# *************************************
+# *************************************
+
+# 3. Practice with strings and lists
+
+# 3.1 Create a variable below, my_name, that holds
+#     a string of either you or your partner's name (e.g. "Emily").
+
+my_name = 'Mikka'
+# Uncomment below to test 3.1
+test('3.1', type(my_name) == str)
+
+
+# 3.2 Create a variable below, our_names, that stores
+#     an array with you and your coding parter's name
+our_names = ['Mikka', 'ChubChub']
+# Uncomment below to test 3.2
+test('3.2', type(our_names) == list and type(our_names[0]) == str and type(our_names[1]) == str)
+
+
+# 3.3 Create a variable, am_in_group, that stores a
+#     boolean indicating if the string my_name is in
+#     the list our_names (hint: use the keyword "in")
+
+am_in_group = my_name in our_names
+# Uncomment below to test 3.3
+test('3.3', type(am_in_group) == bool)
+
+
+# 3.4 Use the list method 'append' to add another name to our_names
+# No test for 3.4 - use 'print' to figure out if it's working as expected
+
+our_names.append('Foo')
+print(our_names)
+# 3.5 Use the list method 'pop' to remove the last name from the end of the list. Store the name you popped off in the variable "removed_from_group"
+# No test for 3.5 - use 'print' to figure out if it's working as expected
+removed_from_group = our_names.pop()
+print(removed_from_group)
+print(our_names)
+
+# *************************************
+# *************************************
+
+# 4. Iteration
+# 4.1 Create a variable my_list that holds the values 1, 18, -3, and 0
+my_list = [1, 18, -3, 0]
+# Uncomment to test Challenge 4.1
+test('4.1', my_list == [1, 18, -3, 0])
+
+
+# 4.2 Create a variable new_list and initialize it to an empty list
+
+new_list = []
+# Uncomment to test Challenge 4.2
+test('4.2', new_list == [])
+
+# 4.3 Create a variable named count and initialize it to zero.
+count = 0
+# Uncomment to test Challenge 4.3
+test('4.3', count == 0)
+
+
+# 4.4 Loop through the variable my_list. During each iteration:
+#     1. print the value of count,
+#     2. increment the value of count, and
+#     3. append the current value of my_list to new_list
+# After running the code, new_list should hold the same values as my_list
+for element in my_list:
+  print(count)
+  count += 1
+  new_list.append(my_list[count-1])
+# Uncomment to test Challenge 4.4
+test('4.4', my_list == new_list)
+
+
+# 4.5 Repeat 4.4, but instead of using the variable count,
+# use the built in python function enumerate
+# No test for 4.4 - use 'print' to see if you are achieving the disired functionality
+for (counter, value) in enumerate(my_list):
+  print(counter, value)
+
+# *************************************
+# *************************************
+
+# 5. Functions
+
+# 5.1 Complete the function, times_two_v1, below.
+# See the instructions to the right for details.
+
+def times_two_v1(num_list):
+  results = []
+  for (counter, value) in enumerate(num_list):
+    results.append(value * 2)
+  return results
+
+# We will test the functions in section 5 a little differently
+# Uncomment the line beginning with 'assert' to test times_two_v1
+# If nothing happens, the test is passing.
+# If an error is thrown, the test isn't passing.
+assert times_two_v1([10,-5, 0, 1]) == [20, -10, 0, 2]
+
+
+# # 5.2 Complete the function zip_up.
+# # See the instructions to the right for details.
+
+# def zip_up(l_1, l_2):
+
+#   combined = []
+#   offset = 0
+#   for counter, value in enumerate(l_1):
+#     combined.append(value)
+#   for counter, value in enumerate(l_2):
+#     combined.insert(counter+1+offset, value)
+#     offset += 1
+#   return combined
+
+# # Uncomment below to test your code
+# assert zip_up([1,2,3], [4,5,6]) == [1,4,2,5,3,6]
+# assert zip_up([1,2], [8,7,6,5]) == [1,8,2,7,6,5]
+
+# 5.3 Complete the function unzip.
+# See the instructions to the right for details.
+
+def unzip(l):
+  evens = l[0:len(l):2]
+  odds = l[1:len(l):2]
+  tuple = ()
+  tuple = (evens, odds)
+  print(tuple)
+  return tuple
+
+# Uncomment following lines to test your code
+# assert unzip([1,2,3,4,5,6]) == ([1,3,5], [2,4,6])
+# assert unzip([7,6,5,4,3,2,1]) == ([7,5,3,1], [6,4,2])
+
+# 5.4 Complete the function mean.
+# See the instructions to the right for details.
+
+# def mean(l):
+#   mean = sum(l)/len(l)
+#   print(mean)
+#   return sum(l)/len(l)
+
+# # Uncomment below to test mean
+# assert mean([5,5,5,5,5]) == 5
+# assert mean([5,20,8,-1,0,0,2,16]) == 6.25
+
+# 5.5 Complete the function std_dev.
+# See the instructions to the right for details.
+import statistics
+
+def std_dev(l):
+  stdev = statistics.stdev(l)
+  print(stdev)
+  return stdev
+
+
+# Uncomment below to test std_dev
+# assert std_dev([5,5,5,5,5]) == 0
+# assert std_dev([5,20,8,-1,0,0,2,16]) == 7.905694150420948
+
+# 5.6 Complete the function mode.
+# See the instructions to the right for details.
+
+import statistics
+def modeOf(l):
+  result = [statistics.mode(l)]
+
+  print(result)
+  return result
+
+# Uncomment below to test mode
+assert modeOf([5,5,5,5,5]) == [5]
+assert modeOf([5,20,8,-1,0,0,2,16]) == [0]
+assert modeOf([2,2,1,1,3]) == [1,2]
+
+# *************************************
+# *************************************
+
+# Congrats on completing Python for Machine Learning part 1!
